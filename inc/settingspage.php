4d5932be9baee900c86c9bef019cdb7e78026b8e
Julian Weiland <jw@werbeagenten.de>
2016-11-15 14:49:30 +0100

    callback url moved to disabled input field

--- inc/settingspage.php
+++ inc/settingspage.php
@@ -51,7 +51,7 @@ class DERWEEILI_MBOT_WOOCOMMERCE_SETTINGS_PAGE {
             'facebook_api_credentials_title' => array(
                 'name'     => __( 'Facebook API Credentials', 'mbot-woocommmerce' ),
                 'type'     => 'title',
-                'desc'     => __( 'Your Callback URL is:', 'mbot-woocommmerce' ) . ' <i>' . plugin_dir_url( __FILE__ ) . 'webhook.php</i>' ,
+                'desc'     => __( 'Your Callback URL is:', 'mbot-woocommmerce' ) . ' <input type="text" disabled>' . plugin_dir_url( __FILE__ ) . 'webhook.php</input>' ,
                 'id'       => 'derweili_mbot_fb_credentials_title'
             ),
             'page_token' => array(
