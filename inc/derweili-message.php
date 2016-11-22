00c262c6160d8ef0647ccf5a9a02db9deff6a48c
derweili <jw@derweili.de>
2016-11-22 02:33:20 +0100

    log messages commented out

--- inc/derweili-message.php
+++ inc/derweili-message.php
@@ -57,8 +57,8 @@ class Der_Weili_Message extends pimax\Messages\Message
 
         //var_dump( $this->get_data_return );
 
-        file_put_contents("log2.html", print_r( $this->get_data_return, true ), FILE_APPEND);
-        file_put_contents("log2.html", print_r( '<hr />', true ), FILE_APPEND);
+        //file_put_contents("log2.html", print_r( $this->get_data_return, true ), FILE_APPEND);
+        //file_put_contents("log2.html", print_r( '<hr />', true ), FILE_APPEND);
 
         return $this->get_data_return;
 
