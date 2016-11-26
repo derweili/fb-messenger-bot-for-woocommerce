8d437e42f87585f2a25a4df5c01143933fab1fb3
derweili <jw@derweili.de>
2016-11-26 09:11:40 +0100

    Uninstall function added

---
+++ uninstall.php
@@ -0,0 +1,43 @@
+<?php
+
+// if uninstall.php is not called by WordPress, die
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
+ 
+
+/**
+* Delete all plugin data on deinstallation
+*/
+class DERWEILI_Mbot_Uninstall
+{
+
+	
+	function __construct()
+	{
+		$this->remove_site_from_whitelisted_domains();
+	}
+
+	function remove_site_from_whitelisted_domains() {
+
+		$settings = new DERWEEILI_MBOT_WOOCOMMERCE_SETTINGS_PAGE();
+		$settings->domain_whitelisting_button('remove');
+
+	}
+
+	funciton remove_options() {
+		
+		delete_option( 'derweili_mbot_page_token' );
+		delete_option( 'derweili_mbot_verify_token' );
+		delete_option( 'derweili_mbot_messenger_app_id' );
+		delete_option( 'derweili_mbot_page_id' );
+
+		delete_option( 'derweili_mbot_checkbox_size' );
+		delete_option( 'derweili_mbot_button_color' );
+		delete_option( 'derweili_mbot_fb_checkbox_prechecked' );
+		delete_option( 'derweili_mbot_fb_checkbox_allow_login' );
+	}
+
+}
+
+new DERWEILI_Mbot_Uninstall();
