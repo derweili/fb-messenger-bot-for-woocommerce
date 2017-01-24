a3bcb345f117c48931dc688ea421964184c7e0ac
derweili <jw@derweili.de>
2017-01-25 00:02:24 +0100

    language code added to js sdk

--- inc/footer-script.php
+++ inc/footer-script.php
@@ -21,7 +21,7 @@ function derweili_footer_script() {
 				var js, fjs = d.getElementsByTagName(s)[0];
 				if (d.getElementById(id)) {return;}
 				js = d.createElement(s); js.id = id;
-				js.src = "//connect.facebook.net/en_US/sdk.js";
+				js.src = "//connect.facebook.net/<?php echo get_locale() ?>/sdk.js";
 				fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
 
